1313541088e529d49dd4e0cb2e036f5424bfd14e
thilothaman10 <thilothaman8@gmail.com>
2025-08-27 15:44:48 +0530

    build

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(helmet());
+//app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginOpenerPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
 app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json({ limit: '50mb' }));
